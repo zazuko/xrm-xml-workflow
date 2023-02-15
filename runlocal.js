1b7ecb3f1b779daa8aabc5884982f592a9e13f30
Cristian <cristianvasquez@gmail.com>
2023-02-15 12:19:58 +0100

    use xrm

--- runlocal.js
+++ runlocal.js
@@ -3,7 +3,7 @@ import { createMapper } from './step.js'
 const triplify = await createMapper()
 
 const result = await triplify({
-  mappingFile: './src-gen/mapping.rml.ttl', outputFile: 'output.ttl',
+  mappingFile: './src-gen/characters.rml.ttl', outputFile: 'output-local.nt',
 })
 
 console.log(result)
